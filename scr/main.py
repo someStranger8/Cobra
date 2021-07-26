b59f95f765f57b706eafdf2d8b61cc943ebc71d8
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-07-26 08:49:46 -0400

    Create main.py

---
+++ scr/main.py
@@ -0,0 +1,56 @@
+import os
+def hackwin():
+    os.system("cat C:\Windows\System32\config")
+def hackmac():
+    os.system("cat /var/db/dslocal/nodes/Default/users")
+def hacklin():
+    os.system("cat /etc/passwd")
+    os.system("cat /etc/shadow")
+def ddos():
+    ddos=input("[*] enter ip or URL: ")
+    os.system("ping "+ ddos+"")
+def sqlinject():
+    os.system("pip3 install sqlmap")
+    clear()
+    os.system("sqlmap --sqlmap-shell")
+def exploit():
+    clear()
+    os.system("msfconsole")
+def shell():
+    i=1
+    while i < 10:
+        dodo=input("root@kali:~# ")
+        os.system(dodo)
+    i+=1
+def hackwifi():
+    os.system("apt install fern-wifi-cracker -y")
+    clear()
+    os.system("fern-wifi-cracker")
+def hackpasswd():
+    os.system("pip3 install h8mail")
+    clear()
+    os.system("h8mail")
+def clear():
+    os.system("clear")
+def mitm():
+    os.system("apt instal wireshark -y")
+    clear()
+    os.system("sudo wireshark")
+def scan():
+    nmap=input("[*] enter ip or URL: ")
+    os.system("apt install nmap -y")
+    clear()
+    os.system("nmap "+ nmap+"")
+def hackweb():
+    os.system("pip3 install websploit")
+    clear()
+    os.system("websploit")
+def hackphone():
+    os.system("apt install scrcpy -y")
+    clear()
+    os.system("scrcpy")
+def hackdrone():
+    os.system("pip3 install dronesploit")
+    clear()
+    os.system("dronesploit")
+print("its time to hack the world")
