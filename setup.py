ed4eaa215e91974be39f8149fedd4bb4e5cbf413
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-07-26 08:59:47 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Cobra",
+    version="1.7",
+    author="Gage Atkinson",
+    author_email="someStranger87@gmail.com",
+    url="https://github.com/someStranger8/PyModule",
+    package_dir={'': 'src'},
+)
